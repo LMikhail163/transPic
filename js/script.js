f466f0a8aa997a357387ba213f8415ed65be54cb
LMikhail163 <81471455+LMikhail163@users.noreply.github.com>
2022-02-21 22:32:04 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/icons/services/arrow1.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/icons/services/arrow2.png"></button>'
+    });
+});
