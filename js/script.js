7582eefd2576bc13d2ddc62c00d9b4192120152e
LMikhail163 <81471455+LMikhail163@users.noreply.github.com>
2022-02-21 22:37:10 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/icons/services/arrow1.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../../icons/icons/services/arrow1.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/icons/services/arrow2.png"></button>'
     });
 });
